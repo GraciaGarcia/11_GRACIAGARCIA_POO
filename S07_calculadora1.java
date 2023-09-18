aea0961b39b3d9fa1f36fa97a5b9c1e32ab7ab1d
GraciaGarcia <140226752+GraciaGarcia@users.noreply.github.com>
2023-09-17 19:28:55 -0500

    Add files via upload

---
+++ S07_calculadora1.java
@@ -0,0 +1,15 @@
+
+package poo.trabajos;
+
+public class calculadora1 {
+    /*ATRIBUTOS*/
+     int a;
+     int b; 
+     
+     /*metodo de parametro*/
+     public int  suma( int a,int b){
+      return a*b;
+     }
+ 
+     
+}
---
+++ S07_calculadora2.java
@@ -0,0 +1,16 @@
+
+package poo.trabajos;
+
+public class calculadora2 {
+    
+      /*ATRIBUTOS*/
+     int a;
+     int b; 
+     
+     /*metodo de retorno*/
+     public int  suma(){
+      return a*b;
+      
+     }
+     
+}
