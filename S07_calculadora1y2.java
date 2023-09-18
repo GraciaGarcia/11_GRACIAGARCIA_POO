4f3e6ea039a3f086c8cfb91e1fb8bf3ec33475a3
GraciaGarcia <140226752+GraciaGarcia@users.noreply.github.com>
2023-09-17 19:23:07 -0500

    Add files via upload

---
+++ S07_calculadora1y2.java
@@ -0,0 +1,14 @@
+
+package poo.trabajos;
+
+public class main {
+
+   
+     
+    public static void main(String[] args) {
+        
+        calculadora1 objT1 = new calculadora1();
+        calculadora2 objT2 = new calculadora2();
+    }
+    
+}
